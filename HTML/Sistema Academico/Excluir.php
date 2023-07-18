33ff2e415d4fd74743efbc1c83696b4cb3ba56d4
arlisson <arlisson234529@gmail.com>
2023-07-18 11:01:53 -0300

    Sistema Academico completo

--- HTML/Sistema Academico/Excluir.php
+++ HTML/Sistema Academico/Excluir.php
@@ -12,6 +12,20 @@ if (!empty($_GET['id'])) {
 
     if ($result->num_rows > 0) {
         try {
+            $sql_atestado = "DELETE FROM atestado WHERE id_pessoa = $id";
+            $delete_atestado = $conexao->query($sql_atestado);
+            $sql_afastamento = "DELETE FROM afastamento WHERE id_pessoa = $id";
+            $delete_afastamento = $conexao->query($sql_afastamento);
+            $sql_reuniao = "DELETE FROM reuniao_pessoa WHERE id_pessoa = $id";
+            $delete_reuniao = $conexao->query($sql_reuniao);
+        } catch (Exception $e) {
+            $_SESSION['msg'] = "<p><h2 class = 'erro'>Erro ao Excluir pessoa: " . $e->getMessage() . "<h2></p>";
+            header("Location:Exibir_Pessoa.php");
+            exit();
+        }
+
+        try {
+
             $sql_code = "DELETE FROM pessoa WHERE id=$id";
             $deletar = $conexao->query($sql_code);
             if (!$deletar) {
--- HTML/Sistema Academico/Inserir_Atestado.php
+++ HTML/Sistema Academico/Inserir_Atestado.php
@@ -41,81 +41,87 @@ if (!empty($_GET['id'])) {
         .sucesso {
             color: green;
         }
+
         body {
-    		border: 1px solid black;
+            border: 1px solid black;
             backgound-color: #CFF4D5;
-		}
+        }
+
         .inside {
-  			border: 1px solid black;
-  			background-color: #CFF4D5; 
-  			padding: 10px;
-		}
+            border: 1px solid black;
+            background-color: #CFF4D5;
+            padding: 10px;
+        }
+
         .primeiro {
-  			border-bottom: 2px solid black;
-  			margin-bottom: 20px; /
-		}
+            border-bottom: 2px solid black;
+            margin-bottom: 20px;/
+        }
+
         .segundo {
-  		margin-top: 20px;
-}
+            margin-top: 20px;
+        }
     </style>
 </head>
 
 <body>
-<div class="inside">
-<div class="primeiro", class="segundo">
-    <h2>Inserir atestados</h2>
-    </div>
-    <?php
-    //se existir mensagem de sessão faça...
-    if (isset($_SESSION['msg'])) {
-        echo $_SESSION['msg'];
-        unset($_SESSION['msg']);
-    }
-
-    ?>
-    <form action="Atestado.php" method="POST">
-        <input type="hidden" value="<?php echo $id ?>" name="id">
-
-        <p>
-            <label style="display: block;">Motivo: </label>
-<textarea placeholder="Digite a Descrição" name="desc" maxlength="240" required style="width: 300px; height: 70px;"></textarea>
-
-
-        </p>
-        <p>
-            <label>Data Inicial:</label>
-            <input type="date" placeholder="Digite a data inicial" name="data_i" required>
-        </p>
-        <p>
-            <label>Data Final:</label>
-            <input type="date" placeholder="Digite a data final" name="data_f" required>
-        </p>
-        <p>
-            <label style="display: block;">Observações: </label>
-<textarea placeholder="Digite a Descrição" name="desc" maxlength="240" required style="width: 300px; height: 70px;"></textarea>
-        </p>
-        <div class="primeiro", class="segundo">
-        <p>
-            <input type="submit" value="Adicionar" name="adicionar">
-
-        </p>
+    <div class="inside">
+        <div class="primeiro" , class="segundo">
+            <h2>Inserir atestados</h2>
         </div>
-        <p>
-            <label>Menu:</label>
-            <select name="menu" onchange="location = this.value;">
-                <option value="">Selecione uma opção...</option>
-                <option value="Menu.php">Menu Principal</option>
-                <option value="Cadastro_Pessoa.php">Cadastrar Pessoa</option>
-                <option value="Listar_Pessoas.php">Listar Pessoas</option>
-                <option value="Exibir_Pessoa.php">Buscar Pessoa</option>
-                <option value="Inserir_Modalidade.php">Cadastrar Modalidade</option>
-                <option value="Listar_Modalidade.php">Listar Modalidade</option>
-                <option value="Registrar_Reuniao.php">Registrar Reunião</option>
-                <option value="Listar_Reuniao.php">Listar Reunião</option>
+        <?php
+        //se existir mensagem de sessão faça...
+        if (isset($_SESSION['msg'])) {
+            echo $_SESSION['msg'];
+            unset($_SESSION['msg']);
+        }
 
-            </select>
-        </p>
-</div>
+        ?>
+        <form action="Atestado.php" method="POST">
+            <input type="hidden" value="<?php echo $id ?>" name="id">
+
+            <p>
+                <label style="display: block;">Motivo: </label>
+                <textarea placeholder="Digite a Descrição" name="motivo" maxlength="240" required
+                    style="width: 300px; height: 70px;"></textarea>
+
+
+            </p>
+            <p>
+                <label>Data Inicial:</label>
+                <input type="date" placeholder="Digite a data inicial" name="data_i" required>
+            </p>
+            <p>
+                <label>Data Final:</label>
+                <input type="date" placeholder="Digite a data final" name="data_f" required>
+            </p>
+            <p>
+                <label style="display: block;">Observações: </label>
+                <textarea placeholder="Digite a Descrição" name="obs" maxlength="240" required
+                    style="width: 300px; height: 70px;"></textarea>
+            </p>
+            <div class="primeiro" , class="segundo">
+                <p>
+                    <input type="submit" value="Adicionar" name="adicionar">
+
+                </p>
+            </div>
+            <p>
+                <label>Menu:</label>
+                <select name="menu" onchange="location = this.value;">
+                    <option value="">Selecione uma opção...</option>
+                    <option value="Menu.php">Menu Principal</option>
+                    <option value="Cadastro_Pessoa.php">Cadastrar Pessoa</option>
+                    <option value="Listar_Pessoas.php">Listar Pessoas</option>
+                    <option value="Exibir_Pessoa.php">Buscar Pessoa</option>
+                    <option value="Inserir_Modalidade.php">Cadastrar Modalidade</option>
+                    <option value="Listar_Modalidade.php">Listar Modalidade</option>
+                    <option value="Registrar_Reuniao.php">Registrar Reunião</option>
+                    <option value="Listar_Reuniao.php">Listar Reunião</option>
+
+                </select>
+            </p>
+    </div>
 </body>
 
 </html>
